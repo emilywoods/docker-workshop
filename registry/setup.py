d6f9b6e6a525e61845fa07fe2c3619fe9e2317bd
Emily Woods <emily@crate.io>
2019-10-22 18:05:03 +0200

    Create starter flask application
    - create flask application with makefile
    - implement register and deregister endpoints
    
    Co-authored-by: Mika Naylor <mika@crate.io>"

---
+++ registry/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="pyladies-devops-workshop",
+    install_requires=["flask==1.1.1"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 1 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
