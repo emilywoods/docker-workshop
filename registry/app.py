01ef5e0c7ff9725640a5d046321e1dd3d019f971
Emily Woods <emily@crate.io>
2019-10-22 18:05:03 +0200

    Create starter flask application
    - create flask application with makefile
    - implement register and deregister endpoints
    
    Co-authored-by: Mika Naylor <mika@crate.io>"

---
+++ registry/app.py
@@ -0,0 +1,38 @@
+from builtins import KeyError
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+REGISTRY = {}
+
+
+@app.route("/")
+def homepage():
+    return "Hello World!"
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    ip_address = request.remote_addr
+    payload = request.get_json(silent=True)
+
+    REGISTRY[ip_address] = {"user": payload["username"], "port": payload["port"]}
+    print("registered users: {}".format(REGISTRY))
+    return "Registering {} with IP Address {} and port {}".format(
+        payload["username"], ip_address, payload["port"]
+    )
+
+
+@app.route("/deregister", methods=["GET", "POST"])
+def deregister():
+    try:
+        REGISTRY.pop("request.remote_addr")
+        return "Deregistered {}".format(request.remote_addr)
+    except KeyError as keyerror:
+        return "{} has no registered containers.".format(request.remote_addr), 404
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0")
