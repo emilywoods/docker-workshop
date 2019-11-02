6d3bbb09c50d4b4f65c917ad77fb6e7655467449
Emily Woods <emilymonica.woods@gmail.com>
2019-11-02 14:54:14 +0100

    set the required python version to > 3.7

--- registry/setup.py
+++ registry/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="pyladies-devops-workshop",
     install_requires=["apscheduler==3.6.1", "flask==1.1.1", "requests==2.22.0"],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 1 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
